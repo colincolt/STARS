b081538e6556f5d1f6c43bce6d9ee5fffb290feb
treencd <46632675+treencd@users.noreply.github.com>
2019-01-29 12:21:18 -0500

    Add files via upload

---
+++ Main File/client_pi.py
@@ -0,0 +1,154 @@
+# /etc/init.d/clientPi.py
+### BEGIN INIT INFO
+# Provides:         clientPi.py
+# Required-Start:   $remote_fs $syslog
+# Required-Stop:    $remote_fs $syslog
+# Default-Start:    2 3 4 5
+# Default-Stop:     016
+# Short-Description:Start daemon at boot time
+# Description:      Enable service provided by daemon.
+### END INIT INFO
+
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+import socket
+import struct
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+
+HOST = '169.254.116.12' # Define the IP address for communication
+PORT = 5025
+BUFFER_SIZE = 1024
+talking = False
+
+serverPi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+while not talking:
+    try:
+        serverPi.connect((HOST, PORT))
+        talking = True
+    except:
+        print('Stereoscopics:   No Server')
+        time.sleep(3)
+        continue
+
+connected = True
+print('connected to serverpi')
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video",
+        help="path to the (optional) video file")
+ap.add_argument("-b", "--buffer", type=int, default=8,
+    help="max buffer size")
+args = vars(ap.parse_args())
+
+# define the lower and upper boundaries of the "red jersey"
+# ball in the HSV color space, then initialize the list of tracked points
+jerseyLower1 = (0, 50, 50)
+jerseyUpper1 = (5, 255, 255)
+jerseyLower2 = (175, 50, 50)
+jerseyUpper2 = (180, 255, 255)
+pts = deque(maxlen=args["buffer"])
+
+framesize = (600, 450)
+frameRate = 10
+# if a video path was not supplied, grab the reference
+# to the webcam
+vs = VideoStream(src=0, resolution = framesize, framerate = frameRate).start()
+
+# allow the camera or video file to warm up
+time.sleep(0.1)
+# keep looping
+while True:
+# grab the current frame
+    start = time.time()
+    frame = vs.read()
+ 
+# handle the frame from VideoCapture or VideoStream
+    #frame = frame[1] if args.get("video", False) else frame
+ 
+# resize the frame, blur it, and convert it to the HSV color space
+    #frame = imutils.resize(frame, width=600)
+    blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+    hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+# construct a mask for the color "red jersey", then perform a series of dilations
+# and erosions to remove any small blobs left in the mask
+    mask0 = cv2.inRange(hsv, jerseyLower1, jerseyUpper1)
+    mask1 = cv2.inRange(hsv, jerseyLower2, jerseyUpper2)
+    mask = mask0 + mask1
+
+    mask = cv2.erode(mask, None, iterations=2)
+    mask = cv2.dilate(mask, None, iterations=2)
+# find contours in the mask and initialize the current(x, y) center of the player
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    center = None
+ 
+# only proceed if at least one contour was found
+    if len(cnts) > 0:
+    # find the largest contour in the mask, then use it to compute the minimum
+    # enclosing circle and centroid
+        c = max(cnts, key=cv2.contourArea)
+        ((x, y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+        centroid = (round((M["m10"] / M["m00"]),3), round((M["m01"] / M["m00"]),3))
+        centroid = (centroid[0]*2464/600, centroid[1]*2464/600)
+    # only proceed if the radius meets a minimum size
+        if radius > 0.1:
+#        # draw the circle and centroid on the frame,then update the list of tracked points
+            cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
+            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+# 
+## update the points queue
+    pts.appendleft(center)
+## loop over the set of tracked points
+    for i in range(1, len(pts)):
+#    # if either of the tracked points are None, ignore them
+            if pts[i - 1] is None or pts[i] is None:
+                    continue
+#    # otherwise, compute the thickness of the line and draw the connecting lines
+            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+            cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+
+    # show the frame to our screen
+    # hypotenuse = ((center[0])**(2)+(center[1])**(2))**(0.5)
+    if len(cnts) > 0:
+        cv2.putText(frame, "Center : " + str(center) , (50,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+#    # cv2.putText(frame, "Hypotenuse : " + str(int(hypotenuse)), (50,100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+        cv2.putText(frame, "Radius : " + str(int(radius)), (50,100), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2)
+#
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    
+    if len(cnts) > 0:
+        value = str(centroid[0])
+        #value = str(x)
+        print ("X-coordinate: " + value)
+        # Send Data and Sockets Reconnection loop
+        try:
+            serverPi.send((value).encode())
+            data = serverPi.recv(BUFFER_SIZE)
+        except socket.error:
+            connected = False
+            serverPi = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            print('connection lost... reconnecting')
+            while not connected:
+                try:
+                    serverPi.connect((HOST, PORT))
+                    connected = True
+                    print('reconnection successful')
+                except socket.error:
+                    time.sleep(2)
+                    
+
+                    
+            
+ 
