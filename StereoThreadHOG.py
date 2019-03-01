8e1bddbe67652fe67b6e5f2ff11b977542d528df
treencd <46632675+treencd@users.noreply.github.com>
2019-03-01 15:04:40 -0500

    Add files via upload

--- StereoThreadHOG.py
+++ StereoThreadHOG.py
@@ -1,5 +1,4 @@
 from threading import Thread
-from multiprocessing import Process, Queue
 import sys
 import numpy as np
 import argparse
@@ -7,6 +6,15 @@ import time
 import socket
 import selectors
 from collections import deque
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import imutils
+import cv2
+ 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 global import_error
 
@@ -69,7 +77,7 @@ while not connected:  # Wait for client
         time.sleep(3)
         continue
 
-def recvData(center,q):
+def recvData():
     try:
         #print('[StereoscopicThread] : waiting for data')
 #            print("test1")
@@ -80,7 +88,7 @@ def recvData(center,q):
 #            print("test3")
         recv_val = data.decode()
 #        print("Client value = " + recv_val)
-        q.put(recv_val)
+        return recv_val
 #            print("test4")
     except socket.error:
         connected = False
@@ -102,7 +110,6 @@ def ProcessLoop(vs, clientPort, BUFFER_SIZE):
     while True:
 #        print("looping")
         start_time = time.time()
-        q = Queue()
 
         ## CHANGE THE WAY YOU ACQUIRE IMAGE
         try:
@@ -130,38 +137,36 @@ def ProcessLoop(vs, clientPort, BUFFER_SIZE):
                     break
 
         
-        blurred = cv2.GaussianBlur(image, (11, 11), 0)
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-        mask0 = cv2.inRange(hsv, jerseyLower1, jerseyUpper1)
-        mask1 = cv2.inRange(hsv, jerseyLower2, jerseyUpper2)
-        mask = mask0 + mask1
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-
-        # find contours in the mask and initialize the current (x, y) center of the ball
-        cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = imutils.grab_contours(cnts)
-        center = None
-
-        # only proceed if at least one contour was found
-        if len(cnts) > 0:
-            # find the largest contour in the mask, then use it to compute the minimum enclosing circle and centroid
-            c = max(cnts, key=cv2.contourArea)
-            ((x, y), radius) = cv2.minEnclosingCircle(c)
-            M = cv2.moments(c)
-            center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-            centroid = (round((M["m10"] / M["m00"]), 3), round((M["m01"] / M["m00"]), 3))
-            centroid = (centroid[0] * 2464 / frame_width, centroid[1] * 2464 / frame_height)
-
-            # only proceed if the radius meets a minimum size
-            if radius > 0.1:
-                # draw the circle and centroid on the frame, then update the list of tracked points
-                cv2.circle(image, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-                cv2.circle(image, center, 5, (0, 0, 255), -1)
-
-        # update the points queue
-        pts.appendleft(center)
+#        image = cv2.imread(imagePath)
+#        image = imutils.resize(image, width=min(400, image.shape[1]))
+#        orig = image.copy()
+     
+        # detect people in the image
+        (rects, weights) = hog.detectMultiScale(image, winStride=(4, 4), padding=(8, 8), scale=1.05)
+     
+        # draw the original bounding boxes
+#        for (x, y, w, h) in rects:
+#            cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+     
+        # apply non-maxima suppression to the bounding boxes using a
+        # fairly large overlap threshold to try to maintain overlapping
+        # boxes that are still people
+        rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+        pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+     
+        # draw the final bounding boxes
+        for (xA, yA, xB, yB) in pick:
+            cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+     
+        # show some information on the number of bounding boxes
+#        filename = imagePath[imagePath.rfind("/") + 1:]
+#        print("[INFO] {}: {} original boxes, {} after suppression".format(
+#            filename, len(rects), len(pick)))
+     
+        # show the output images
+#        cv2.imshow("Before NMS", orig)
+        cv2.imshow("Frame", image)
+        cv2.waitKey(0)
         
         #print("test1")
         # loop over the set of tracked points
@@ -175,55 +180,33 @@ def ProcessLoop(vs, clientPort, BUFFER_SIZE):
 #            thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
 #            cv2.line(image, pts[i - 1], pts[i], (0, 0, 255), thickness)
         #print("test2")
-        tcp = Process(target=recvData, args = (center,q))
-        tcp.start()
-        compvalue = q.get(True)
+#        compvalue = recvData()
 #        print("Client value = " + compvalue)
-
-#        try:
-#            #print('[StereoscopicThread] : waiting for data')
-##            print("test1")
-#            data = clientPort.recv(BUFFER_SIZE)
-##            print("test2")
-#            #print("[StereoscopicThread] : received data:", data)
-#            clientPort.send(data)  # SEND DATA BACK (COULD BE USED FOR STOP COMMAND)
-##            print("test3")
-#            compvalue = data.decode()
-#            print(compvalue)
-##            print("test4")
-#        except socket.error:
-#            connected = False
-#            while not connected: #and not shutdown_event.isSet() and not kill_event.isSet():
-#                try:
-#                    data = clientPort.recv(BUFFER_SIZE)
-#                    print("[StereoscopicThread] : received data:", data)
-#                    clientPort.send(data)  # SEND DATA BACK (COULD BE USED FOR STOP COMMAND)
-#                    compvalue = data.decode()
-#                    print(compvalue)
-#                    connected = True
-#                except socket.error:
-#                    print("[Stereo] : Lost the client connection")
-#                    time.sleep(0.5)
                     
         cv2.imshow("Frame", image)
         key = cv2.waitKey(1) & 0xFF
         
-        if len(cnts) > 0:
-            #print("Decoded value: " + compvalue)
-            slaveval = float(compvalue)
-            masterval = centroid[0]
-            disparity = abs(masterval - slaveval)
-            distance = (focalsize * baseline) / (disparity * pixelsize)
-            # SENDS DATA TO Class, which can be "Put" using Stacks's______________________________
-            # results = StereoOutput
-            # results.distance = distance
-            # results.disparity = disparity
-            # results.masterval = masterval
-            # results.slaveval = slaveval
-            # stereoStack.push(results)
-            fps = time.time() - start_time
-            print("[Stereo] :   FPS =  " + str(fps) + "||    Distance:  " + str(distance))
-        
+#        if len(cnts) > 0:
+#            #print("Decoded value: " + compvalue)
+#            slaveval = float(compvalue)
+#            masterval = centroid[0]
+#            disparity = abs(masterval - slaveval)
+#            distance = (focalsize * baseline) / (disparity * pixelsize)
+#            # SENDS DATA TO Class, which can be "Put" using Stacks's______________________________
+#            # results = StereoOutput
+#            # results.distance = distance
+#            # results.disparity = disparity
+#            # results.masterval = masterval
+#            # results.slaveval = slaveval
+#            # stereoStack.push(results)
+#            fps = time.time() - start_time
+#            print("[Stereo] :   FPS =  " + str(fps) + "||    Distance:  " + str(distance))
+
+        fps = time.time() - start_time
+        print("[Stereo] :   FPS =  " + str(fps) + "||    Distance:  " + str(distance))
+
+
+
 class PiVideoStream:
     def __init__(self, resolution=(frame_width, frame_height), framerate=32):
         # initialize the camera and stream
@@ -288,3 +271,4 @@ vs = PiVideoStream().start()
 time.sleep(2.0)
 
 ProcessLoop(vs, clientPort, BUFFER_SIZE)
+
