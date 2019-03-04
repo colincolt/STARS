d77e0d3935ea016099b7d1d04f4fb3e4ab91caab
treencd <46632675+treencd@users.noreply.github.com>
2019-03-04 11:21:40 -0500

    Add files via upload

--- Launcher_Test.py
+++ Launcher_Test.py
@@ -2,6 +2,8 @@ import random
 import serial
 import serial.tools.list_ports
 import sys
+import numpy as np
+
 
 
 def findMEGA():
@@ -11,18 +13,31 @@ def findMEGA():
             return (p[0])
     return ('NULL')
 
+def findUNO():
+    ports = list(serial.tools.list_ports.comports())
+    for p in ports:
+        # print(p)           # This causes each port's information to be printed out.
+        if "0043" in p[2]:
+            return (p[0])
+    return ('NULL')
+
 
 def startup():
     mega_port = findMEGA()
+    uno_port = findUNO()
+
     try:
         MEGA = serial.Serial(mega_port, 115200, timeout=1)
+        UNO = serial.Serial(uno_port, 115200, timeout=1)  # change ACM number as found from "ls /dev/tty*"
+
     except FileNotFoundError as e:
         print("findMEGA returned 'NULL' looking for Arduino Mega")
     except serial.SerialException as ex:
         pass
     else:
         MEGA.baudrate = 115200
-        return MEGA
+        UNO.baudrate = 115200
+        return MEGA, UNO
 
 def wait_for_data(MEGA):
     getdata = False
@@ -67,6 +82,18 @@ def get_input(MEGA):
 
     difficulty = "1"
 
+    pitchAngleTable = np.array([[5, 15],  # << Pitch angle lookup table based on estimations
+                                     [7, 16],
+                                     [9, 17],
+                                     [11, 18],
+                                     [13, 19],
+                                     [15, 20],
+                                     [17, 21],
+                                     [19, 22],
+                                     [21, 23],
+                                     [23, 24],
+                                     [25, 25]])
+
     RPM = -1.13635244 * used_distance ** 2.0 + 97.7378699 * used_distance + 646.034298  # <-- Polynomial fit
     motor_speed = round((RPM / 5000) * 255)  # Value between 0-255 (On 24 V: 0-5000 RPM)
     targetChoice = int(random.choice([1, 2 , 3, 4]))
@@ -76,8 +103,21 @@ def get_input(MEGA):
     send_data = '<' + motor_speed + ',' + motor_speed + ',' + str(targetChoice) + ',' + str(
         difficulty) + ',' + ballfeed + ',' + str(estimated_tof) + '>'
 
+    # Query table for angle at self.usedDistance
+    row = round((used_distance - 0.99) / 2) - 2
+    if row < 0:
+        row = 0
+    elif row > 10:
+        row = 10
+    pitchAngle = pitchAngleTable[row, 1]
+
+    uno_data = '<0,'+str(pitchAngle)+'>'
+
     print("Send this data to Motors?")
+    print("Launcher:")
     print(send_data)
+    print("Pitch Angle:")
+    print(uno_data)
     send = input("y/n:")
 
     if send == "y":
@@ -90,7 +130,7 @@ def get_input(MEGA):
             sys.exit()
 
 
-MEGA = startup()
+MEGA, UNO = startup()
 
 while True:
-    get_input(MEGA)
+    get_input(MEGA, UNO)
